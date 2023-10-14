454fcd5de8bd1beda3f61e2d2eb446d5f7613920
Pham Tan Hung WSL <chonmaiut160904@gmail.com>
2023-10-14 17:18:06 +0700

    Edit

---
+++ LAB_01/LAB_01/src/FirstDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you1");
+        System.exit(0);
+
+    }
+}
